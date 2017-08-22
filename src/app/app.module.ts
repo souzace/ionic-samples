8d7fb1f8ca60ce85dec584fd63d89845db29ec00
Fabio Souza <souzace@gmail.com>
2017-08-22 18:57:58 -0300

    exemplo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,45 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
+import { IntroPage } from "../pages/intro/intro";
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { FeedPageModule } from "../pages/feed/feed.module";
-import { IntroPageModule } from "../pages/intro/intro.module";
 import { HttpModule } from "@angular/http";
 import { MovieProvider } from '../providers/movie/movie';
+import { FeedPage } from "../pages/feed/feed";
 
 @NgModule({
   declarations: [
     MyApp,
+    IntroPage,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    FeedPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    FeedPageModule,
-    IntroPageModule,
     HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    IntroPage,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
-    
+    ListPage,
+    FeedPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    MovieProvider
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    
   ]
 })
 export class AppModule {}
